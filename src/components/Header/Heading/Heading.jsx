dde4224cd83fdc30ac739058ceb8160e99c1539e
emiaesthetic <emi.aesthetic@gmail.com>
2025-01-20 18:33:27 +0500

    feat: Add propTypes validation to Heading

--- src/components/Header/Heading/Heading.jsx
+++ src/components/Header/Heading/Heading.jsx
@@ -1,5 +1,10 @@
+import PropTypes from 'prop-types';
 import style from './Heading.module.css';
 
 export const Heading = ({ children }) => (
   <h1 className={style.heading}>{children}</h1>
 );
+
+Heading.propTypes = {
+  children: PropTypes.node.isRequired,
+};
