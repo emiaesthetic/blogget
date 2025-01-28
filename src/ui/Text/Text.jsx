d2d6a192e0c72f9c52c963f8c8a3715b5d09f6d1
emiaesthetic <emi.aesthetic@gmail.com>
2025-01-28 11:07:53 +0500

    feat: Add UI component Text

---
+++ src/ui/Text/Text.jsx
@@ -0,0 +1,48 @@
+import classNames from 'classnames';
+import style from './Text.module.css';
+import PropTypes from 'prop-types';
+
+export const Text = prop => {
+  const {
+    As = 'span',
+    color = 'black',
+    size,
+    tsize,
+    dsize,
+    className,
+    href,
+    center,
+    children,
+  } = prop;
+
+  const classes = classNames(
+    className,
+    style[color],
+    { [style[`fs${size}`]]: size },
+    { [style[`fst${tsize}`]]: tsize },
+    { [style[`fsd${dsize}`]]: dsize },
+    { [style['center']]: center },
+  );
+
+  return (
+    <As className={classes} href={href}>
+      {children}
+    </As>
+  );
+};
+
+Text.propTypes = {
+  As: PropTypes.string,
+  color: PropTypes.string,
+  size: PropTypes.number,
+  tsize: PropTypes.number,
+  dsize: PropTypes.number,
+  className: PropTypes.string,
+  href: PropTypes.string,
+  center: PropTypes.bool,
+  children: PropTypes.oneOfType([
+    PropTypes.string,
+    PropTypes.object,
+    PropTypes.array,
+  ]),
+};
