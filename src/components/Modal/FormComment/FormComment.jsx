49dfa29e31f774b4b84a3327fedbe4274ec03a0a
emiaesthetic <emi.aesthetic@gmail.com>
2025-02-01 15:33:25 +0500

    feat: Add button to toggle comment form

--- src/components/Modal/FormComment/FormComment.jsx
+++ src/components/Modal/FormComment/FormComment.jsx
@@ -1,23 +1,40 @@
-import { useRef } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import style from './FormComment.module.css';
 import { Text } from '../../../ui/Text';
 
 export const FormComment = () => {
+  const [isFormVisible, setIsFormVisible] = useState(false);
   const commentRef = useRef(null);
 
   const handleSubmit = event => {
     event.preventDefault();
     console.log(commentRef.current.value);
     commentRef.current.value = '';
+    setIsFormVisible(false);
   };
 
+  useEffect(() => {
+    if (isFormVisible && commentRef.current) {
+      commentRef.current.focus();
+    }
+  }, [isFormVisible]);
+
   return (
-    <form className={style.form} onSubmit={handleSubmit}>
-      <Text As="h3" size={14} tsize={18}>
-        Имя авторизованного пользователя
-      </Text>
-      <textarea className={style.textarea} ref={commentRef}></textarea>
-      <button className={style.btn}>Отправить</button>
-    </form>
+    <>
+      {!isFormVisible && (
+        <button className={style.btn} onClick={() => setIsFormVisible(true)}>
+          Написать комментарий
+        </button>
+      )}
+      {isFormVisible && (
+        <form className={style.form} onSubmit={handleSubmit}>
+          <Text As="h3" size={14} tsize={18}>
+            Имя авторизованного пользователя
+          </Text>
+          <textarea className={style.textarea} ref={commentRef}></textarea>
+          <button className={style.btn}>Отправить</button>
+        </form>
+      )}
+    </>
   );
 };
